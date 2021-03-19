d529b4dc6b044ef5fdf6dab883adacd925757208
mnmeet-singh <59867171+mnmeet-singh@users.noreply.github.com>
2021-03-19 14:17:36 +0530

    Update Grip_bank.session.sql

--- Grip_bank.session.sql
+++ Grip_bank.session.sql
@@ -1,9 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 19, 2020 at 10:01 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
